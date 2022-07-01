d5ab60958ffd28cf65490bb5bd8fdc84baff3c6e
Kevin Evans <kevinevansfinlengr@gmail.com>
2022-07-01 18:19:18 -0400

    Initial Commit

---
+++ TLArt/TLArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  TLArtApp.swift
+//  TLArt
+//
+//  Created by KEVIN EVANS on 7/1/22.
+//
+
+import SwiftUI
+
+@main
+struct TLArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
